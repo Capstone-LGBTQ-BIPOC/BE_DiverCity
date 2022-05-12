19e7ba60983bc16c9bbf4d160a6f57b47dcca69f
Joseph W Galvin <jwgalvin@gmail.com>
2022-05-12 17:22:00 -0600

    Feat:yelp_service added and passes tests

---
+++ app/services/yelp_service.rb
@@ -0,0 +1,20 @@
+class YelpService
+  class << self
+    def yelp_search(location)
+      response = conn.get("/v3/businesses/search?find_desc=lgbt+or+black+owned+businesses&location=#{location}") do |f|
+        f.headers['Authorization'] = ENV['yelp_key']
+      end
+      parse_data(response)
+    end
+
+
+    def conn
+      Faraday.new(url: "https://api.yelp.com")
+    end
+
+    def parse_data(response)
+      data = JSON.parse(response.body, symbolize_names: true)
+    end
+
+  end
+end
