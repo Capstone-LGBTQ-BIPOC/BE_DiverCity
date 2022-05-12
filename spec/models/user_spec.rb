e8b31d32c633044b226243d7d5e563e8735a7263
Tamara Dowis <tamara.dowis@gmail.com>
2022-05-12 13:38:45 -0500

    test: user model validations

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:user_name) }
+    it { should validate_uniqueness_of(:user_name) }
+  end
+end
