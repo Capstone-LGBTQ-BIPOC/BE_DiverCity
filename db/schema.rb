072b05c88d9fc335f93e3b50711d1abd9b81846a
Joseph W Galvin <jwgalvin@gmail.com>
2022-05-12 16:31:03 -0600

    added route template for endpoints.

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_512_175_959) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2022_05_12_175959) do
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'user_name'
-    t.string 'zipcode'
-    t.string 'city'
-    t.string 'state'
-    t.string 'street_address'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "zipcode"
+    t.string "city"
+    t.string "state"
+    t.string "street_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
