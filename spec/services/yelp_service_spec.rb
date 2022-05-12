e38e8c5e13255344670adc51ae116736ff1214f8
Joseph W Galvin <jwgalvin@gmail.com>
2022-05-12 17:15:27 -0600

    Test: service spec pending FE key needs

--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -10,11 +10,17 @@ RSpec.describe YelpService do
   it 'gets a reponse with data I need', :vcr do
     response = YelpService.yelp_search("denver")
     expect(response).to be_a(Hash)
-    binding.pry
-    expect(response.count).to eq(1)
+    #binding.pry
+    expect(response.count).to eq(3)
     expect(response).to have_key(:businesses)
+    expect(response).to have_key(:total)
+    expect(response).to have_key(:region)
     expect(response[:businesses]).to have_key(:location)
     expect(response[:businesses]).to have_key(:total)
     expect(response[:businesses]).to have_key(:phone)
+    expect(response[:businesses]).to have_key(:name)
+    expect(response[:businesses]).to have_key(:image_url)
+    expect(response[:businesses]).to have_key(:url)
+    expect(response[:businesses]).to have_key(:name)
   end
 end
