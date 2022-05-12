7975e5f27e58705490c492079f32d74acb18e0b4
Tamara Dowis <tamara.dowis@gmail.com>
2022-05-12 14:56:04 -0500

    appease rubocop, tests still passing

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    user_name { "MyString" }
-    zipcode { "MyString" }
-    city { "MyString" }
-    state { "MyString" }
-    street_address { "MyString" }
+    user_name { 'MyString' }
+    zipcode { 'MyString' }
+    city { 'MyString' }
+    state { 'MyString' }
+    street_address { 'MyString' }
   end
 end
