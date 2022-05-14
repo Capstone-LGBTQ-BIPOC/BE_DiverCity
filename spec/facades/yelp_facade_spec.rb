a8d0859c71e7108d6566ce43fe4ed4cf65b86fb5
Joseph W Galvin <jwgalvin@gmail.com>
2022-05-14 13:20:47 -0600

    test: adding robustness

--- spec/facades/yelp_facade_spec.rb
+++ spec/facades/yelp_facade_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Yelpfacade do
+RSpec.describe YelpFacade do
   it 'has a response', :vcr do
     response = YelpFacade.location_search("denver")
-    binding.pry
     expect(response).to be_a(Array)
+    expect(response.count).to eq(20)
+    response.each do |business|
+      expect(business).to be_a(List)
+      expect(business.name).to be_a(String)
+      expect(business.address).to be_a(String)
+      expect(business.sub_category).to be_a(Array)
+      expect(business.id).to be_a(String)
+      expect(business.image).to be_a(String)
+    end
   end
+
+  
 end
