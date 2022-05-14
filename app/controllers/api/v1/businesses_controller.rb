19c6eecf36ffededea04d323dbfc39eaf95df094
Joseph W Galvin <jwgalvin@gmail.com>
2022-05-14 16:31:45 -0600

    Minor changes

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -2,6 +2,5 @@ class Api::V1::BusinessesController < ApplicationController
   def index
     businesses = YelpFacade.find_category(params[:location], params[:category])
     render json: BusinessListSerializer.new(businesses)
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+get 'api/v1/businesses', to: "businesses#index"
   namespace :api do
     namespace :v1 do
       resources :businesses, only: [:index]
