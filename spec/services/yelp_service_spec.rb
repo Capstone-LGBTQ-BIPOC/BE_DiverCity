a34853a5fdf609119ab911167a0e3adbfacf3580
Joseph W Galvin <jwgalvin@gmail.com>
2022-05-14 13:30:01 -0600

    commented out prys removed

--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -4,23 +4,19 @@ RSpec.describe YelpService do
   it 'has a response', :vcr do
     response = YelpService.conn
     expect(response).to be_a(Faraday::Connection)
-    #binding.pry
   end
 
   it 'gets a reponse with data in the right format', :vcr do
     response = YelpService.yelp_search("denver")
     expect(response).to be_a(Hash)
-    #binding.pry
     expect(response.count).to eq(3)
     expect(response).to have_key(:businesses)
     expect(response).to have_key(:total)
     expect(response).to have_key(:region)
-
   end
 
   it 'gets a reponse with data with the keys needed', :vcr do
     response = YelpService.yelp_search("denver")
-
     expect(response[:businesses].count).to eq(20)
     expect(response[:businesses][0]).to have_key(:location)
     expect(response[:businesses][0]).to have_key(:categories)
@@ -33,7 +29,6 @@ RSpec.describe YelpService do
 
   it 'search by category does the voodoo it needs to', :vcr do
     response = YelpService.search_by_category("denver", "music")
-    #binding.pry
     expect(response[:businesses].count).to eq(20)
     expect(response[:businesses][0]).to have_key(:location)
     expect(response[:businesses][0]).to have_key(:categories)
