96a3a2bb9f66f299515ad6a0b7802bda845445f1
Joseph W Galvin <jwgalvin@gmail.com>
2022-05-19 15:38:06 -0600

    minor changes

--- app/facades/yelp_facade.rb
+++ app/facades/yelp_facade.rb
@@ -11,7 +11,7 @@ class YelpFacade
       business_list2 = YelpService.search_lgbt_owned(location, category)
       business_poro = poro_ize(business_list)
       business_poro2 = poro_ize(business_list2)
-      zippered_list = business_poro2.zip(business_poro).flatten.compact
+      business_poro2.zip(business_poro).flatten.compact
     end
 
     def find_business(id)
