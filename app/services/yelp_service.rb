78adaf2730e28bc46f221e2548be8f953fabb55b
Tamara Dowis <tamara.dowis@gmail.com>
2022-05-19 13:49:11 -0500

    update code for linter approval

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -2,33 +2,31 @@ class YelpService
   class << self
     def yelp_search(location)
       response = conn.get("/v3/businesses/search?find_desc=lgbt+or+black+owned+businesses&location=#{location}") do |f|
-        f.headers['Authorization'] = ENV['yelp_key']
+        f.headers['Authorization'] = ENV.fetch('yelp_key', nil)
       end
       parse_data(response)
     end
 
     def search_lgbt_owned(location, category)
       response = conn.get("/v3/businesses/search?location=#{location}&category=#{category}&term=lgbtq+owned") do |f|
-        f.headers['Authorization'] = ENV['yelp_key']
+        f.headers['Authorization'] = ENV.fetch('yelp_key', nil)
       end
       parse_data(response)
     end
 
     def search_black_owned(location, category)
       response = conn.get("/v3/businesses/search?location=#{location}&term=#{category}+black+owned") do |f|
-        f.headers['Authorization'] = ENV['yelp_key']
+        f.headers['Authorization'] = ENV.fetch('yelp_key', nil)
       end
       parse_data(response)
     end
 
-
     def conn
-      Faraday.new(url: "https://api.yelp.com")
+      Faraday.new(url: 'https://api.yelp.com')
     end
 
     def parse_data(response)
-      data = JSON.parse(response.body, symbolize_names: true)
+      JSON.parse(response.body, symbolize_names: true)
     end
-
   end
 end
