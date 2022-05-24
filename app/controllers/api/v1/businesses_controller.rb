54a7167cc1980ca3a1a19f11697b2bf4bbca5e5a
Tamara Dowis <tamara.dowis@gmail.com>
2022-05-24 16:05:24 -0500

    add city not found error handling

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::BusinessesController < ApplicationController
   def index
     businesses = YelpFacade.find_category(params[:location], params[:category])
-
-    render json: BusinessListSerializer.new(businesses)
+    if businesses.class == Array
+      render json: BusinessListSerializer.new(businesses)
+    elsif businesses.class == Hash
+      render json: ErrorSerializer.api_format(businesses), status: 404
+    end
   end
 
   def show
